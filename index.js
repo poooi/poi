67322b4e8dbcba5b62decca20f9ef457aa5b9fad
KochiyaOcean <root@kochiyaocean.org>
2016-07-23 16:30:05 +0800

    Rewrite app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('coffee-script/register');
-require('babel-register')(require('./babel.config'));
-require('./lib/cl');
-require('./app');
+require('coffee-script/register')
+require('babel-register')(require('./babel.config'))
+require('./lib/cl')
+require('./app')
