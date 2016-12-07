05c9f18d5c76ead25e2a60750aa63ac5889d59b9
Dazzy Ding <h@dazzyd.org>
2016-12-07 18:57:35 +0800

    Update fcd build script

--- fcd/build.js
+++ fcd/build.js
@@ -1,31 +1,43 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 const CSON = require('cson')
 
 function build_map(dest) {
   fs.writeFileSync(
     path.join(dest, 'mapspot.json'),
-    JSON.stringify(CSON.load('./mapspot.cson'))
+    JSON.stringify(CSON.load('mapspot.cson'))
   )
   fs.writeFileSync(
     path.join(dest, 'maproute.json'),
-    JSON.stringify(CSON.load('./maproute.cson'))
+    JSON.stringify(CSON.load('maproute.cson'))
+  )
+}
+
+function build_meta(dest) {
+  const meta = []
+  for (const fpath of fs.walkSync(dest)) {
+    const fdata = JSON.parse(fs.readFileSync(fpath))
+    meta.push(fdata.meta)
+  }
+  fs.writeFileSync(
+    path.join(dest, 'meta.json'),
+    JSON.stringify(meta)
   )
 }
 
 function build() {
-  const dest = './build'
-  try {
-    fs.mkdirSync(dest)
-  }
-  catch (err) {
-    if (err.code != 'EEXIST')
-      console.error(err.stack)
-  }
+  const dest = '../assets/data/fcd'
 
+  // Remove old
+  fs.emptyDirSync(dest)
+
+  // Build JSON
   build_map(dest)
+
+  // Build meta
+  build_meta(dest)
 }
 
 build()
