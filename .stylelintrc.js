4af6797d124d2f4f8e89da973e91c3c69af9b4dc
Kagami <moe.pascal@gmail.com>
2018-02-10 16:24:32 +0800

    lint existing css files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: 'stylelint-config-standard',
+  rules: {
+    'selector-type-no-unknown': [true, {
+      ignoreTypes: [
+        'kan-game',
+        '/^poi-/',
+      ],
+    }]
+  },
 }
