edc32e5b77874e8bee6b9fe73e402e1be0a396e9
KochiyaOcean <root@kochiyaocean.org>
2018-02-19 18:59:54 +0900

    Update to babel@7

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  presets: ["es2017-node7", "stage-0", "react"],
+  presets: [
+    ["env", {
+      "targets": {
+        "node": "8.2",
+      },
+    }], "stage-0", "react"],
   plugins: [
-    "add-module-exports",
     "dynamic-import-node",
-    "closure-elimination",
-    "transform-decorators",
   ],
-  ignore: false,
-  only: /.es$/,
+  only: [/.es$/],
 }
