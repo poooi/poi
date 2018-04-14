dc1d2467c1b48592749ab77c3e7a2c9dd235b331
Jennings Wu <jenningswu@gmail.com>
2018-04-14 15:07:46 -0700

    Only hack document.write for the game page

--- assets/js/webview-preload.js
+++ assets/js/webview-preload.js
@@ -146,13 +146,15 @@ const handleDOMContentLoaded = () => {
 
 document.addEventListener("DOMContentLoaded", handleDOMContentLoaded)
 
-const _documentWrite = document.write
 
-document.write = function() {
-  if (document.readyState === 'interactive' || document.readyState === 'complete') {
-    console.warn(`Block document.write since document is at state "${document.readyState}". Blocked call:`, arguments)
-  } else {
-    _documentWrite.apply(this, arguments)
+if (window.location.toString().includes("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/")) {
+  const _documentWrite = document.write
+  document.write = function() {
+    if (document.readyState === 'interactive' || document.readyState === 'complete') {
+      console.warn(`Block document.write since document is at state "${document.readyState}". Blocked call:`, arguments)
+    } else {
+      _documentWrite.apply(this, arguments)
+    }
   }
 }
 
