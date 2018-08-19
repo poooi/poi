af2122e6a597aa7611ff51d5f3f4a9d11e26a792
KochiyaOcean <root@kochiyaocean.org>
2018-08-19 14:59:09 +0900

    Fix: crash on certain device

--- app.js
+++ app.js
@@ -89,10 +89,10 @@ if (process.platform === 'win32') {
 app.commandLine.appendSwitch('js-flags', "--harmony --harmony-do-expressions")
 
 // Fix GPU acceleration
-app.commandLine.appendSwitch('enable-accelerated-2d-canvas', 'true')
+// app.commandLine.appendSwitch('enable-accelerated-2d-canvas', 'true')
 app.commandLine.appendSwitch('enable-gpu-rasterization', 'true')
 app.commandLine.appendSwitch('enable-native-gpu-memory-buffers', 'true')
-app.commandLine.appendSwitch('enable-surface-synchronization', 'true')
+// app.commandLine.appendSwitch('enable-surface-synchronization', 'true')
 app.commandLine.appendSwitch('enable-checker-imaging', 'true')
 
 // Cache size
