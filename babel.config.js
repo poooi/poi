740d41a8da8b7bf7ae9168f012e198420ac4cf82
Kagami <moe.pascal@gmail.com>
2018-09-13 23:33:07 +0800

    chore: babel 7 upgrade

--- babel.config.js
+++ babel.config.js
@@ -4,24 +4,40 @@ module.exports = {
       require.resolve('@babel/preset-env'),
       {
         targets: {
-          electron: '2.0',
+          electron: '3.0',
         },
         loose: true,
       },
     ],
-    [
-      require.resolve('@babel/preset-stage-0'),
-      {
-        decoratorsLegacy: true,
-        pipelineProposal: 'minimal',
-      },
-    ],
     require.resolve('@babel/preset-react'),
   ],
-  plugins: [
-    require.resolve('babel-plugin-dynamic-import-node'),
-    require.resolve('babel-plugin-add-module-exports'),
-  ],
+  plugins:[
+    [
+      require.resolve('@babel/plugin-proposal-decorators'),
+      { legacy: true },
+    ],
+    [
+      require.resolve('@babel/plugin-proposal-pipeline-operator'),
+      { proposal: 'minimal' },
+    ],
+  ].concat([
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-do-expressions',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-json-strings',
+    '@babel/plugin-proposal-logical-assignment-operators',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    'babel-plugin-add-module-exports',
+    'babel-plugin-dynamic-import-node',
+  ].map(plugin => require.resolve(plugin))),
   ignore: [],
   only: [/.es$/],
   babelrc: false,
