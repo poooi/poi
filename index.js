f9266361163434bed5f28ebce0f170804287bc63
KochiyaOcean <root@kochiyaocean.org>
2018-11-25 23:24:52 +0900

    chore: dont require babel in main process if is packaged

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-require('@babel/register')(require('./babel.config'))
+if (require.resolve('./lib/cli').endsWith('es')) {
+  require('@babel/register')(require('./babel.config'))
+}
 require('./lib/cli')
 require('./app')
