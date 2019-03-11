3b1e952170b735051c7462f6984aa9396e0526d6
Kagami <moe.pascal@gmail.com>
2019-03-11 22:52:21 +0800

    chore: set prettier format error level to warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
     ],
     'import/no-named-as-default-member': [0],
     'react-hooks/rules-of-hooks': 'error',
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'warn',
   },
   settings: {
     'import/resolver': {
