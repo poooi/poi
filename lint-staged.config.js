e215d4f2229d871fcad14d295d04ed7bb895c81c
東風谷オーシャン <root@kochiyaocean.org>
2019-10-06 00:25:10 +0900

    fix: lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{es,js}': ['eslint --fix', 'stylelint --config .stylelintrc.styled.js', 'git add'],
+  '*.{es,js}': ['eslint --fix', 'stylelint --config .stylelintrc.js', 'git add'],
   '*.css': ['stylelint --config .stylelintrc.css.js'],
   '*.md': ['prettier --write', 'git add'],
 }
