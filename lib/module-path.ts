0af50bb45042b504262824ca7928a85fc97ae973
東風谷オーシャン <root@kochiyaocean.org>
2022-08-19 21:24:33 +0900

    fix: original module path should have higher priority

--- lib/module-path.ts
+++ lib/module-path.ts
@@ -1,20 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore: module seems to be special
 import { Module } from 'module'
-import path from 'path'
 
 // @ts-ignore: patching internal variables
 const { _nodeModulePaths } = Module
 
 const allowedPaths = new Set<string>()
-
-/**
- * this is the same function from views/utils, we cannot share because we want to also use it in main
- */
-function isSubdirectory(parent: string, dir: string) {
-  const relative = path.relative(parent, dir)
-  return !relative || (!relative.startsWith('..') && !path.isAbsolute(relative))
-}
 
 /**
  * we search for a package in node modules, we will node_modules at every level
@@ -31,13 +22,7 @@ export const setAllowedPath = function (...modulePaths: string[]) {
   Module._nodeModulePaths = function (from: string) {
     // use function style instead of arrows, expecting some possible perf gain
 
-    // putting allowed path in front so that main program's module path has higher priority
-    return allowedPathsArray.concat(
-      _nodeModulePaths.call(this, from).filter(function (dir: string) {
-        return allowedPathsArray.some(function (parent) {
-          return isSubdirectory(parent, dir)
-        })
-      }),
-    )
+    // putting allowed path in back so that main program's module path has lower priority
+    return _nodeModulePaths.call(this, from).concat(allowedPathsArray)
   }
 }
