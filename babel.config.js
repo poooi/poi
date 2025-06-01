2f3106ad2f9ffba0aa9765fd0ef58816324288bb
かがみ <moe.pascal@gmail.com>
2025-06-01 18:59:29 +0800

    feat: clean up obselete babel plugins (#2585)

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
       require.resolve('@babel/preset-env'),
       {
         targets: {
-          electron: '15.1',
+          electron: '36.3',
         },
         loose: true,
       },
@@ -15,23 +15,14 @@ module.exports = {
   plugins: [
     require.resolve('babel-plugin-styled-components'),
     [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-    [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
     [require.resolve('@babel/plugin-proposal-pipeline-operator'), { proposal: 'minimal' }],
   ].concat(
     [
       '@babel/plugin-proposal-do-expressions',
       '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-export-namespace-from',
       '@babel/plugin-proposal-function-bind',
       '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-json-strings',
-      '@babel/plugin-proposal-logical-assignment-operators',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
-      '@babel/plugin-proposal-numeric-separator',
-      '@babel/plugin-proposal-optional-chaining',
       '@babel/plugin-proposal-throw-expressions',
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-syntax-import-meta',
       'babel-plugin-add-module-exports',
       'babel-plugin-dynamic-import-node',
     ].map((plugin) => require.resolve(plugin)),
