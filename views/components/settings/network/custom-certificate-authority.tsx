1775320439262103a82c42566c049df7082a00c2
かがみ <moe.pascal@gmail.com>
2025-10-26 23:20:28 +0800

    feat: add ca cert test

--- views/components/settings/network/custom-certificate-authority.tsx
+++ views/components/settings/network/custom-certificate-authority.tsx
@@ -5,6 +5,9 @@ import { Button, Callout, Intent } from '@blueprintjs/core'
 import { get } from 'lodash'
 import { useSelector } from 'react-redux'
 import { FileFilter } from 'electron'
+import fs from 'fs-extra'
+import { X509Certificate } from 'crypto'
+import assert from 'assert'
 
 const filters: FileFilter[] = [{ name: 'PEM', extensions: ['pem'] }]
 
@@ -19,6 +22,24 @@ export const CustomCertificateAuthority = () => {
     config.set('poi.network.customCertificateAuthority', '')
   }, [])
 
+  const handleTest = useCallback(async () => {
+    try {
+      const ca = await fs.readFile(value, 'utf8')
+      const cert = new X509Certificate(ca)
+      assert(cert.ca)
+      window.toast(t('setting:Certificate is valid'), {
+        type: 'success',
+        title: t('setting:Custom Certificate Authority test'),
+      })
+    } catch (e) {
+      console.error(e)
+      window.toast(t('setting:Certificate is invalid'), {
+        type: 'error',
+        title: t('setting:Custom Certificate Authority test'),
+      })
+    }
+  }, [value, t])
+
   return (
     <div>
       <FolderPickerConfig
@@ -28,9 +49,14 @@ export const CustomCertificateAuthority = () => {
         filters={filters}
         extraControl={
           value && (
-            <Button intent={Intent.DANGER} minimal onClick={handleDelete}>
-              {t('setting:Delete')}
-            </Button>
+            <>
+              <Button intent={Intent.PRIMARY} minimal onClick={handleTest}>
+                {t('setting:Test')}
+              </Button>
+              <Button intent={Intent.DANGER} minimal onClick={handleDelete}>
+                {t('setting:Delete')}
+              </Button>
+            </>
           )
         }
       />
