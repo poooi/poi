6e2ee8a292de3151419a0ac2dd314714a3fde846
東風谷オーシャン <root@kochiyaocean.org>
2026-05-07 22:56:00 +0900

    chore: ignore non-src test files

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ module.exports = {
     '^views/(.*)': '<rootDir>/views/$1',
   },
   testMatch: ['**/__tests__/**/*.[ejt]s?(x)', '**/?(*.)+(spec|test).[ejt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/app_compiled/'],
   setupFilesAfterEnv: ['./setup-tests.ts'],
   collectCoverageFrom: ['lib', 'views', 'build'].map((dir) => `./${dir}/**/*.[ejt]s?(x)`),
   collectCoverage: _.toLower(process.env.CI) === 'true',
